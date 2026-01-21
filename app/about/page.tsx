2b4cd0ab20872954251c0dbe324f1c5e32ccbd6a
Pablo Gutiérrez Sánchez <pabgut02@ucm.es>
2026-01-21 15:02:05 +0100

    Fin del módulo 1

---
+++ app/about/page.tsx
@@ -0,0 +1,20 @@
+export default function AboutPage() {
+  return (
+    <main className="max-w-3xl mx-auto p-6">
+      <h1 className="text-3xl font-bold mb-4">
+        Museo Metropolitano de Arte (MET)
+      </h1>
+
+      <p className="mb-4 text-gray-700">
+        El <strong>Museo Metropolitano de Arte</strong>, conocido como el MET,
+        es uno de los museos más importantes del mundo y se encuentra en la
+        ciudad de Nueva York.
+      </p>
+
+      <p className="text-gray-700">
+        Fue fundado en 1870 y alberga millones de obras de arte de distintas
+        culturas y períodos históricos.
+      </p>
+    </main>
+  );
+}
---
+++ app/components/header.tsx
@@ -0,0 +1,8 @@
+// app/components/Header.tsx
+export default function Header() {
+  return (
+    <header className="bg-blue-900 text-white p-4">
+      <h1>Museo MET</h1>
+    </header>
+  );
+}
